c28b074f8ac6ebbce8fb668e43d5b98b980a668e
Amaury Carrade <amaury@carrade.eu>
2018-06-26 00:31:40 +0200

    Added support for tags lists and other taxonomy thingy

--- nebulius.php
+++ nebulius.php
@@ -9,4 +9,39 @@ class Nebulius extends Theme
     public $features = [
         'blueprints' => 1000,
     ];
+
+    public static function getSubscriptedEvents()
+    {
+        return [
+            'onThemesInitialized' => ['onThemesInitialized', 0]
+        ];
+    }
+
+    public function onPluginsInitialized()
+    {
+        if ($this->isAdmin()) return;
+
+        $this->enable([
+            'onTwigExtensions' => ['onTwigExtensions', 0]
+        ]);
+    }
+
+    public function onTwigExtensions()
+    {
+        /** @var $twig \Twig_Environment */
+        $twig = $this->grav['twig']->twig;
+
+        $twig->addFilter(new \Twig_SimpleFilter('shuffle', function($array)
+        {
+            $keys = array_keys($array);
+            shuffle($keys);
+
+            $new = [];
+            foreach ($keys as $key) {
+                $new[$key] = $array[$key];
+            }
+
+            return $new;
+        }));
+    }
 }
